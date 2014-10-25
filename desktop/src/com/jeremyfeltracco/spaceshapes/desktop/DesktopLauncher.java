08304cc083cff769feb7fbd89f47066fe977ade7
Jeremy Feltracco <jpfeltracco@gmail.com>
2014-10-25 12:45:22 -0400

    Initial commit

---
+++ desktop/src/com/jeremyfeltracco/spaceshapes/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jeremyfeltracco.spaceshapes.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jeremyfeltracco.spaceshapes.SpaceShapes;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShapes(), config);
+	}
+}
