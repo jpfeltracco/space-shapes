9e114d33fc1ce80ac4f5afe8dbb51b7061cfb755
Jeremy Feltracco <jpfeltracco@gatech.edu>
2014-10-26 22:53:56 -0400

    Adds auto packing which closes issue #2

--- core/src/com/jeremyfeltracco/spaceshapes/SpaceShapes.java
+++ core/src/com/jeremyfeltracco/spaceshapes/SpaceShapes.java
@@ -3,22 +3,37 @@ package com.jeremyfeltracco.spaceshapes;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 
 public class SpaceShapes extends Game {
     UserInterface ui;
+    TextureAtlas atlas;
+    Sprite pentagon;
+    SpriteBatch batch;
 
 	@Override
 	public void create () {
         ui = new UserInterface();
         ui.create();
+
+        batch = new SpriteBatch();
+        atlas = new TextureAtlas("packedAssets/packed.atlas");
+        pentagon = atlas.createSprite("pentagon");
+
 	}
 
 	@Override
 	public void render () {
         // Clear the screen
-		Gdx.gl.glClearColor(0.5f, 0.5f, 0, 1);
+		Gdx.gl.glClearColor(0.23f, 0.23f, 0.23f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        pentagon.draw(batch);
+        batch.end();
 
         ui.render(); // Render the UI last
 	}
--- desktop/src/com/jeremyfeltracco/spaceshapes/desktop/DesktopLauncher.java
+++ desktop/src/com/jeremyfeltracco/spaceshapes/desktop/DesktopLauncher.java
@@ -2,10 +2,14 @@ package com.jeremyfeltracco.spaceshapes.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.jeremyfeltracco.spaceshapes.SpaceShapes;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+        // This packs our assets on every run into "packedAssets/packed.atlas"
+        TexturePacker.process("../images/toPack", "packedAssets", "packed");
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new SpaceShapes(), config);
 	}
